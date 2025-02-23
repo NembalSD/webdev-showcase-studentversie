658f5e93bdc0ae6ea6be11b94fc077d3e61ed78c
NembalSD <robanvisser@gmail.com>
2025-02-23 16:17:15 +0100

    mailAPI implementatie en stukje opmaak

--- UC2-Contactpagina/ShowcaseAPI/Controllers/MailController.cs
+++ UC2-Contactpagina/ShowcaseAPI/Controllers/MailController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShowcaseAPI.Models;
-
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ShowcaseAPI.Controllers
@@ -15,7 +17,21 @@ namespace ShowcaseAPI.Controllers
         {
             //Op brightspace staan instructies over hoe je de mailfunctionaliteit werkend kunt maken:
             //Project Web Development > De showcase > Week 2: contactpagina (UC2) > Hoe verstuur je een mail vanuit je webapplicatie met Mailtrap?
-            
+            try
+            {
+                var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+                {
+                    Credentials = new NetworkCredential("42f55069816e02", "a38c586cd76afb"),
+                    EnableSsl = true
+                };
+
+                client.Send(form.Email, "to@example.com", "Hello world", "testbody");
+                Debug.WriteLine("Sent");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Fout bij verzenden mail: {ex.Message}");
+            }
             return Ok();
         }
     }
